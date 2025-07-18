337e6521803ee40ab4172da7fce7479381e5acaa
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-18 23:19:11 +0700

    del-vmess.sh

--- vmess/del-vmess.sh
+++ vmess/del-vmess.sh
@@ -12,24 +12,24 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#@ " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "                ${WB}Delete Vmess Account${NC}                "
+echo -e "                  ${WB}Hapus Akun Vmess${NC}                  "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "  ${YB}You have no existing clients!${NC}"
+echo -e "  ${YB}Belum ada pengguna yang terdaftar!${NC}"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Tekan tombol apa saja untuk kembali ke menu"
 vmess
 fi
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "                ${WB}Delete Vmess Account${NC}                "
+echo -e "                  ${WB}Hapus Akun Vmess${NC}                  "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}User  Expired${NC}  "
+echo -e " ${YB}Pengguna Kadaluwarsa${NC}  "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 grep -E "^#@ " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
 echo ""
-echo -e "${YB}tap enter to go back${NC}"
+echo -e "${YB}Tekan Enter untuk kembali ke menu${NC}"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-read -rp "Input Username : " user
+read -rp "Masukkan Nama Pengguna : " user
 if [ -z $user ]; then
 vmess
 else
@@ -40,13 +40,13 @@ rm -rf /user/log-vmess-$user.txt
 systemctl restart xray
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "           ${WB}Vmess Account Success Deleted${NC}            "
+echo -e "           ${WB}Akun Vless Berhasil Dihapus${NC}            "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}Client Name :${NC} $user"
-echo -e " ${YB}Expired On  :${NC} $exp"
+echo -e " ${YB}Nama Pengguna   :${NC} $user"
+echo -e " ${YB}Berlaku Sampai  :${NC} $exp"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Tekan tombol apa saja untuk kembali ke menu"
 clear
 vmess
 fi

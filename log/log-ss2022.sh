a400f41cced1df4fe1c52a665285183427be48c6
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-19 01:41:54 +0700

    log-ss2022.sh

--- log/log-ss2022.sh
+++ log/log-ss2022.sh
@@ -12,31 +12,31 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^#% " "/usr/local/etc/xray/config.json")
 if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "            ${WB}Log Shadowsocks 2022 Account${NC}            "
+echo -e "           ${WB}Catatan Akun Shadowsocks 2022${NC}            "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "  ${YB}You have no existing clients!${NC}"
+echo -e "  ${YB}Belum ada pengguna yang terdaftar!${NC}"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Tekan tombol apa saja untuk kembali ke menu"
 log-create
 fi
 clear
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "            ${WB}Log Shadowsocks 2022 Account${NC}            "
+echo -e "           ${WB}Catatan Akun Shadowsocks 2022${NC}            "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e " ${YB}User  Expired${NC}  "
+echo -e " ${YB}Daftar Pengguna & Tanggal Berakhir${NC}  "
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
 grep -E "^#% " "/usr/local/etc/xray/config.json" | cut -d ' ' -f 2-3 | column -t | sort | uniq
 echo ""
-echo -e "${YB}tap enter to go back${NC}"
+echo -e "${YB}tekan enter untuk kembali${NC}"
 echo -e "${BB}————————————————————————————————————————————————————${NC}"
-read -rp "Input Username : " user
+read -rp "Masukan Nama Pengguna : " user
 if [ -z $user ]; then
 log-create
 else
 clear
 echo -e "`cat "/user/log-ss2022-$user.txt"`"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Tekan tombol apa saja untuk kembali ke menu"
 log-create
 fi

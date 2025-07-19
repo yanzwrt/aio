6d4f36ecdb86097d1c00218280cb2b84b3b73975
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-19 15:22:39 +0700

    cek-vless.sh

--- vless/cek-vless.sh
+++ vless/cek-vless.sh
@@ -10,9 +10,9 @@ WB='\e[37;1m'
 clear
 echo -n >/tmp/other.txt
 data=($(cat /usr/local/etc/xray/config.json | grep '^#=' | cut -d ' ' -f 2 | sort | uniq))
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
-echo -e "         ${WB}Daftar Pengguna Vless yang Login${NC}           "
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}══════════════════════════════════════════════════════${NC}"
+echo -e "       ${WB}★ Daftar Pengguna Vless yang Login ★${NC}"
+echo -e "${BB}══════════════════════════════════════════════════════${NC}"
 for akun in "${data[@]}"; do
 if [[ -z "$akun" ]]; then
 akun="Tidak Ada"
@@ -36,7 +36,7 @@ else
 jum2=$(cat /tmp/ipvmess.txt | nl)
 echo "user : $akun"
 echo "$jum2"
-echo -e "${BB}————————————————————————————————————————————————————${NC}"
+echo -e "${BB}══════════════════════════════════════════════════════${NC}"
 fi
 rm -rf /tmp/ipvmess.txt
 done
